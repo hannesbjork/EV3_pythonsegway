38e659eb1abd1eaa044df822c754f713d6267689
Hannes Björk <hannes.b97@gmail.com>
2021-04-12 15:22:30 +0200

    Extended test-cases, restructuring

--- test.py
+++ test.py
@@ -1,43 +1,104 @@
-#!/usr/bin/env python3
+#!/usr/bin/env micropython
 import time
 from time import sleep
+
 import ev3dev.ev3
-import struct
+from ev3dev.ev3 import *
 
+import math
 
-leftWheelA           = ev3dev.ev3.LargeMotor('outA')
-rightWheelD          = ev3dev.ev3.LargeMotor('outD')
+leftWheel = ev3dev.ev3.LargeMotor('outD')
+rightWheel = ev3dev.ev3.LargeMotor('outA')
 
-portACG = LegoPort(address='ev3-ports:in2')
-portACG.mode = 'nxt-i2c'
+sensorACG = ev3dev.ev3.Sensor(address="in2")
+sensorACG.mode = "TILT"
 
-sensorACG = Sensor(address='ev3-ports:in2:i2c17')
+samp_time = 75; #in ms
 
-motorinput = 20
+theta_star = 40
+
+U_MAX = 90
+U_MIN = -90
+
+g = 9.816
+
+K = 20
+D = 1
+
+#Timed sampling test
+next_time = int(round(time.time() * 1000)) + samp_time
+while True:
+
+    sensorACG.mode = "TILT"
+    theta = sensorACG.value(2) - theta_star
+
+    sensorACG.mode = "GYRO"
+    theta_dot = sensorACG.value(2) - theta_star
+
+    u = g * math.sin(math.radians(theta)) * K - D * theta_dot
+
+    if u > U_MAX:
+        u = U_MAX
+    elif u < U_MIN:
+        u = U_MIN
+
+    leftWheel.run_direct(duty_cycle_sp = u)
+    rightWheel.run_direct(duty_cycle_sp = u)
+
+    current_time = int(round(time.time() * 1000))
+
+    if current_time < next_time:
+        sleep((next_time-current_time)/1000)
+    else:
+        print("Lagging behind!")
+    
+    next_time = next_time + samp_time
+
+motorinput = 50
 
 #######################
 
-sensorACG.mode = "TILT"
-print("tilt-x "+sensorACG.value(0))
-print("tilt-y "+sensorACG.value(1))
-print("tilt-z "+sensorACG.value(2))
+#Bang-bang control test
+while False: 
+    if sensorACG.value(2) < 33:
+        leftWheel.run_direct(duty_cycle_sp = -motorinput)
+        rightWheel.run_direct(duty_cycle_sp = -motorinput)
+    elif sensorACG.value(2) > 33:
+        leftWheel.run_direct(duty_cycle_sp = motorinput)
+        rightWheel.run_direct(duty_cycle_sp = motorinput)
 
-sensorACG.mode = "ACCEL"
-print("accel-x: "+sensorACG.value(0))
-print("accel-y: "+sensorACG.value(1))
-print("accel-z: "+sensorACG.value(2))
+rightWheel.stop()
+leftWheel.stop()
 
-sensorACG.mode = "GYRO"
-print("gyro-x: "+sensorACG.value(0))
-print("gyro-y: "+sensorACG.value(1))
-print("gyro-z: "+sensorACG.value(2))
+#Sensor test
+while False:
 
-leftWheelA.run_direct(duty_cycle_sp = motorinput)
-rightWheelD.run_direct(duty_cycle_sp = motorinput)
+    sensorACG.mode = "TILT"
+    print("tilt-x "+str(sensorACG.value(0)))
+    print("tilt-y "+str(sensorACG.value(1)))
+    print("tilt-z "+str(sensorACG.value(2)))
+
+    sensorACG.mode = "ACCEL"
+    print("accel-x: "+str(sensorACG.value(0)))
+    print("accel-y: "+str(sensorACG.value(1)))
+    print("accel-z: "+str(sensorACG.value(2)))
+
+    sensorACG.mode = "GYRO"
+    print("gyro-x: "+str(sensorACG.value(0)))
+    print("gyro-y: "+str(sensorACG.value(1)))
+    print("gyro-z: "+str(sensorACG.value(2)))
+
+    sleep(1)
+
+#leftWheel.run_direct(duty_cycle_sp = motorinput)
+#rightWheel.run_direct(duty_cycle_sp = motorinput)
 
 sleep(1)
 
-leftWheelA.run_direct(duty_cycle_sp = -motorinput)
-rightWheelD.run_direct(duty_cycle_sp = -motorinput)
+#leftWheel.run_direct(duty_cycle_sp = -motorinput)
+#rightWheel.run_direct(duty_cycle_sp = -motorinput)
 
 sleep(1)
+
+#rightWheel.stop()
+#leftWheel.stop()
