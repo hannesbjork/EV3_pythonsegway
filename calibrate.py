43fb326968283a39dec25eb26e81c62523987964
Hannes Björk <hannes.b97@gmail.com>
2021-04-25 12:18:17 +0200

    Reworking control and sensor/motor use, cleanup

---
+++ calibrate.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+############################################################### Imports
+
+import ev3dev.ev3 as ev3
+from ev3dev.ev3 import *
+import math
+import time
+import sys
+import importlib
+from time import sleep
+
+############################################################### Inits
+
+#Init gyro
+gyroSensor = ev3.Sensor(address="in3")
+gyroSensorValueRaw  = open(str(gyroSensor._path) + "/value0", "rb") 
+
+############################################################### Functions
+
+def get_time():
+    return int(round(time.time() * 1000))
+
+def get_read(infile):
+    infile.seek(0)    
+    return(int(infile.read().decode().strip()))
+
+def get_bodyangspeed(): 
+    gyroRateRaw = get_read( gyroSensorValueRaw)
+    return gyroRateRaw
+
+############################################################### Variables
+
+#Count loops
+loops = 0
+max_loops = 50
+
+#Total calib
+GYRO_OFFSET = 0
+
+#Time variables
+sample_time = 30 #in ms
+sample_time_s = sample_time/1000 #in s
+
+#Set first time
+next_time = get_time() + sample_time
+
+############################################################### Calibration loop
+while loops < max_loops:
+
+    bodyAngSpeed = get_bodyangspeed()
+    print(bodyAngSpeed)
+
+    GYRO_OFFSET = GYRO_OFFSET + bodyAngSpeed/max_loops
+
+    loops = loops + 1
+
+    #Loop timing
+    diff_time = next_time - get_time()
+    if diff_time > 0:
+        sleep(diff_time/1000)
+    else:
+        print("Missed deadline: "+str(diff_time)+"ms")
+        
+    next_time = next_time + sample_time
+
+CALIBRATION_PATH = "segway_calibration.txt"
+calib = open(CALIBRATION_PATH,'w') 
+calib.write(str(GYRO_OFFSET)) 
+calib.close()
--- main.py
+++ main.py
@@ -1,138 +1,148 @@
 #!/usr/bin/env python3
 
-#Imports
-import ev3dev.ev3
+############################################################### Imports
+
+import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 import math
 import time
+import sys
+import importlib
 from time import sleep
-import struct
+
+############################################################### Inits
 
 #Init motors
-wheel_left = ev3dev.ev3.LargeMotor('outD')
-wheel_right = ev3dev.ev3.LargeMotor('outA')
-count_per_rot = (wheel_left.count_per_rot + wheel_right.count_per_rot)/2
+motorLeft = ev3.LargeMotor('outD')
+motorRight = ev3.LargeMotor('outA') 
 
-#Init sensor
-sensor_ACG = ev3dev.ev3.Sensor(address="in3")
-#sensor_ACG.mode = "ALL" #TODO
-sleep(0.05)
+motorLeft.reset()              
+motorRight.reset()
+motorLeft.run_direct()
+motorRight.run_direct()
 
-#Compensation variables
-comp_bodyang=0
-comp_wheelang=0
+motorEncoderLeft    = open(str(motorLeft._path) + "/position", "rb")    
+motorEncoderRight   = open(str(motorRight._path) + "/position", "rb")  
+    
+motorDutyCycleLeft = open(str(motorLeft._path) + "/duty_cycle_sp", "w")
+motorDutyCycleRight= open(str(motorRight._path) + "/duty_cycle_sp", "w")
 
-#Limit variables
+#Init gyro
+gyroSensor = ev3.Sensor(address="in3")
+gyroSensorValueRaw  = open(str(gyroSensor._path) + "/value0", "rb") 
+
+#Init button
+touchSensor = ev3.Sensor(address="in2")
+
+############################################################### Constants
+
 U_MAX = 100 #in % of max
 U_MIN = -100 #in -% of max
 ANG_MAX = 40
+MOTOR_VOLTAGE_PERCENT = 0.75 #7.5V / 100%
+RAD_PER_DEG = 3.14159/180 
 
-#Functions
+############################################################### Get latest calibration
+
+CALIBRATION_PATH = "segway_calibration.txt"
+calib = open(CALIBRATION_PATH, 'r')
+
+GYRO_OFFSET_raw = calib.readline()
+GYRO_OFFSET_raw.replace(" ", "")
+GYRO_OFFSET = float(GYRO_OFFSET_raw)
+
+calib.close()
+
+############################################################### Functions
+
+#Read from sensor and motor file
+def get_read(infile):
+    infile.seek(0)    
+    return(int(infile.read().decode().strip()))
+
+#Write to motors    
+def set_write(outfile,value):
+    outfile.truncate(0)
+    outfile.write(str(int(value)))
+    outfile.flush() 
+
+#Get time in ms
 def get_time():
     return int(round(time.time() * 1000))
 
-def get_sensordata():
-    #(ang, angvel) = struct.unpack('<2xb18xh', sensor_ACG.bin_data())
-    #return ((math.asin(ang/128)*180/math.pi)-comp_bodyang, (angvel*0.00875))
-    gyro = sensor_ACG.value() 
-    return (gyro-2951)*0.2084
 
-def get_motordata():
-    pos = (wheel_left.position + wheel_right.position)/(4)
-    vel = (wheel_left.speed + wheel_right.speed)/(4)
+def get_bodyangspeed(): 
+    gyroRateRaw = get_read( gyroSensorValueRaw)
+    gyroRate = (gyroRateRaw - GYRO_OFFSET)*0.2084*RAD_PER_DEG
+    return gyroRate
 
-    return (-(pos+comp_wheelang), -vel)
+def get_wheelposition():
+    motorAngleRaw = (get_read(motorEncoderLeft) + get_read(motorEncoderRight))/2
+    motorAngle = motorAngleRaw*RAD_PER_DEG
+    return motorAngle
 
-def set_output(u, angle):
-    if abs(angle)>ANG_MAX:
-        wheel_left.stop()
-        wheel_right.stop()
-    else:
-        wheel_left.run_direct(duty_cycle_sp = -u)
-        wheel_right.run_direct(duty_cycle_sp = -u)
+def stop_motors():
+    motorLeft.stop()
+    motorRight.stop()
 
+def set_duty(motorDutyFilePath, u):
 
-#Set compensation vars
-#comp_bodyang = get_sensordata()[0]
-comp_bodyang = get_sensordata()
-comp_wheelang = get_motordata()[0]
-
-#Control variables
-sample_time = 30 #in ms
-
-K = 0.075 #scalar for the motor output, 7.5V / 100%
-K = 0.1
-
-theta_param = -36.1829   #K1
-psi_param = -194.3097 #K2 
-theta_dot_param = -4.1627  #K3
-psi_dot_param = -15.2284 #K4
-
-
-#theta_param = -1.0035  #K1
-#psi_param = 158.7342 #K2 
-#theta_dot_param = -1.4785  #K3
-#psi_dot_param = -10.4721 #K4
-
-#theta_param = -5.9552 #K1
-#psi_param = -38.7172 #K2 
-#theta_dot_param = -1.3539  #K3
-#psi_dot_param = -2.8635 #K4
-
-#theta_param = -0.0010 #K1
-#psi_param = 254.3399 #K2 
-#theta_dot_param = -0.0476  #K3
-#psi_dot_param = -12.5356 #K4
-
-#theta_param = -7  #K1
-#psi_param = 1700 #K2 
-#theta_dot_param = -9  #K3
-#psi_dot_param = 120 #K4
-
-#Temp vars TODO
-psi = 0
-theta = 0
-psi_dot = 0
-theta_dot = 0
-psi_old = 0
-theta_old = 0
-dummy = 0
-
-#Control loop
-next_time = get_time() + sample_time
-while True:
-
-    #Testing TODO
-    psi_old = psi
-    theta_old = theta
-
-    #Get sensor values
-    
-    psi_dot = get_sensordata()
-    psi = psi + psi_dot*sample_time/1000
-    
-    #(psi, psi_dot) = get_sensordata()
-    (theta, theta_dot) = get_motordata()
-
-    #Testing TODO
-    #psi_dot = ((psi-psi_old)*4 + psi_dot)/5
-    #theta_dot = ((theta-theta_old)*2 + theta_dot)/3
-
-    #Debug TODO
-    print("psi:"+ str(math.trunc(psi))+" psi*:"+ str(math.trunc(psi_dot))+" theta:"+ str(math.trunc(theta))+" theta*:"+ str(math.trunc(theta_dot)))
-
-    #Control algorithm
-    u = K*(theta_dot_param*theta_dot -psi_param*psi - psi_dot_param*psi_dot +theta_param*theta  )
-    #u = 0 #Temp TODO
+    u_int = -int(round(u*MOTOR_VOLTAGE_PERCENT))
 
     #Saturation
-    if u > U_MAX:
-        u = U_MAX
-    elif u < U_MIN:
-        u = U_MIN
+    if u_int > U_MAX:
+        u_int = U_MAX
+    elif u_int < U_MIN:
+        u_int = U_MIN
 
-    #Set output signal and give signal so that a limit can be enforced
-    set_output(u, psi)
+    # Apply the signal to the motor
+    set_write(motorDutyFilePath, u_int)
+
+
+############################################################### Control vars
+
+#Time variables
+sample_time = 30 #in ms
+sample_time_s = sample_time/1000 #in s
+
+#Control parameters
+param_wheelang  = -7  #K1
+param_bodyang = 1700 #K2 
+param_wheelangspeed = -9  #K3
+param_bodyangspeed = 120 #K4
+
+#Establish recurring variables
+wheelAng_old = 0
+wheelAngSpeed = 0
+wheelAng = 0
+bodyAng = 0
+
+#Set first time
+next_time = get_time() + sample_time
+
+Sound.beep()
+
+############################################################### Control loop
+while True:
+
+    wheelAng_old = wheelAng
+    wheelAng = get_wheelposition()
+    bodyAngSpeed = get_bodyangspeed()
+
+    bodyAng = bodyAng + bodyAngSpeed*sample_time_s
+    wheelAngSpeed = (wheelAng-wheelAng_old)/sample_time_s
+
+    #Debug TODO
+    print("wheelAng:"+ str(math.trunc(wheelAng))+" bodyAng:"+ str(math.trunc(bodyAng))+" wheelAngSpeed:"+ str(math.trunc(wheelAngSpeed))+" bodyAngSpeed:"+ str(math.trunc(bodyAngSpeed)))
+
+    #Control algorithm
+    u = ( param_wheelang * wheelAng 
+        + param_bodyang * bodyAng 
+        + param_wheelangspeed * wheelAngSpeed 
+        + param_bodyangspeed * bodyAngSpeed )
+
+    set_duty(motorDutyCycleRight, u)
+    set_duty(motorDutyCycleLeft , u)
 
     #Loop timing
     diff_time = next_time - get_time()
@@ -142,4 +152,19 @@ while True:
         print("Missed deadline: "+str(diff_time)+"ms")
         
     next_time = next_time + sample_time
+
+    #Button reset
+    while touchSensor.value():
+
+        motorLeft.reset()              
+        motorRight.reset()
+        motorLeft.run_direct()
+        motorRight.run_direct()
+
+        set_duty(motorDutyCycleRight, 0)
+        set_duty(motorDutyCycleLeft , 0)
+        bodyAng=0
+        time.sleep(0.1)
+        next_time = get_time() + sample_time
+  
 
